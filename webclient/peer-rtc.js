71c94351afc71b463a0793bb0de9660f2b033a77
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-06-09 17:59:24 +0800

    Update peer-rtc.js

--- webclient/peer-rtc.js
+++ webclient/peer-rtc.js
@@ -2,19 +2,32 @@ class PeerRTC {
 	
 	constructor(myId, serverURL) {
 		this.serverURL = serverURL
-		this.getApi(`${serverURL}/clientConnect`)
+
+		var queryParams = `key=${this.uuidv4()}`
+		if (myId != null && myId != undefined && myId.trim() != "") {
+			queryParams+=`&id=${myId}`
+		}
+		this.id = this.getApiJSON(`${serverURL}/registerClient?${queryParams}`).id
+		console.log(this.id)
 	}
 
 	
 
-	 getApi(theUrl, data=null){
+	 getApiJSON(theUrl){
 	    var xmlHttp = new XMLHttpRequest()
 	    xmlHttp.open("GET", theUrl, false)
-	    xmlHttp.send(data)
-	    return xmlHttp.responseText
+	    xmlHttp.send(null)
+	    return JSON.parse(xmlHttp.responseText)
 	}
 
 
+
+	uuidv4() {
+	  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+		return v.toString(16);
+	  });
+	}
 	
 }
 
