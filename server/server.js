04a0acb3596a582c069ab8d0782d56194a838708
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-06-10 16:43:20 +0800

    Move host and port to variable

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
+const HOST = "127.0.0.1"
+const PORT = 1000
+
 const ws = require("ws")
 const signaling = require("./signaling.js")
 
-const server = new ws.Server({port:1000})
+const wsserver = new ws.Server({host:HOST, port:PORT})
+const httpserver = require("express")()
 
-server.on("connection", client=>{
+
+wsserver.on("connection", client=>{
 	signaling.addNewClient(client)
 })
+
