4c7e44d3361b8890e325da64769f30500f2b78be
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-06-10 17:47:34 +0800

    Remove http server

--- server/server.js
+++ server/server.js
@@ -4,9 +4,8 @@ const PORT = 1000
 const ws = require("ws")
 const signaling = require("./signaling.js")
 
-const wsserver = new ws.Server({host:HOST, port:PORT})
-const httpserver = require("express")()
 
+const wsserver = new ws.Server({host:HOST, port:PORT})
 
 wsserver.on("connection", client=>{
 	signaling.addNewClient(client)
