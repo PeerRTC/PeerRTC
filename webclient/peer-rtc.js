f671b6497e0174e55f5b0aa14dd27d8f00692986
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-06-10 12:15:35 +0800

    peer rtc now connect via socket to the server

--- webclient/peer-rtc.js
+++ webclient/peer-rtc.js
@@ -2,49 +2,13 @@ class PeerRTC {
 	
 	constructor(myId, serverURL) {
 		this.serverURL = serverURL
-		this.key = this.randomKey(100, 200)
 		
-		var queryParams = `key=${this.key}`
-		if (myId != null && myId != undefined && myId.trim() != "") {
-			queryParams+=`&id=${myId}`
-		}
-		this.id = this.getApiJSON(`${serverURL}/registerClient?${queryParams}`).id
-		this.getHook()
+		const socket = new WebSocket(serverURL)
+		this.socket = socket
 	}
 
 	
-	getHook(){
-		new Promise(r=>{
-			while(true){
-				console.log(this.getApiJSON(`${this.serverURL}/hook`))
-			}
-		})
-	}
-
-	 getApiJSON(url){
-	    var xmlHttp = new XMLHttpRequest()
-	    xmlHttp.open("GET", url, false)
-	    xmlHttp.send(null)
-	    return JSON.parse(xmlHttp.responseText)
-	}
-
-
-
-	randomKey(minLen, maxLen) {
-	  const data = "abcdefghijklmnopqrstuvwxyz-_0123456789" 
-	  var key = ""
-	  for(var i=0; i< Math.floor(Math.random() * maxLen) + minLen ; i++){
-	  	var index = Math.floor(Math.random() * data.length)
-	  	var d = data[index]
-	  	
-	  	if (Math.random() <= 0.5) {
-	  		d = d.toUpperCase()
-	  	}
-	  	key+=d
-	  }
-
-	  return key
-	}
+	
 	
 }
 
