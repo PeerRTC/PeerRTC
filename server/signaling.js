b5b5f66ba7729932debdd3a2dbf8d88d13e3a0d6
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-06-10 15:18:13 +0800

    Added message handler

--- server/signaling.js
+++ server/signaling.js
@@ -4,14 +4,6 @@ const resBuilder = require("./response-builder")
 const clients = new Map()
 
 function addNewClient(client){
-	client.on("message", data=>{
-
-	})
-
-	client.on("close", () => {
-		clients.delete(client)
-	})
-
 	var id = null
 
 	// prevent id duplicates
@@ -19,18 +11,59 @@ function addNewClient(client){
 		id = utils.uuidv4()
 	}
 
+	client.on("message", data=>{
+		handleMessage(id, data)
+	})
+
+	client.on("close", () => {
+		clients.delete(client)
+	})
+
+	
+
 	const metadata = {
-		id: id,
+		client: client,
 		lastUpdateTime: utils.getNowMillis()
 	}
 
-	clients.set(client, metadata)
+	clients.set(id, metadata)
 
 	const res = new resBuilder.ResponseBuilder()
-	res.buildTypeInitial(metadata.id, metadata.lastUpdateTime)
+	res.buildTypeInitial(id, metadata.lastUpdateTime)
 	client.send(res.getResponse())
 }
 
+
+function handleMessage(requesterId, data){
+	try{
+		const jsonData = JSON.parse(data)
+		const res = new resBuilder.ResponseBuilder()
+
+		if (jsonData.type == "connectpeer") {
+			const peerId = jsonData.peerId
+
+			// Request connection only if not connecting to itself or peer target exists
+			if (requesterId != peerId && clients.has(peerId)) {
+				const peerClient = clients.get(peerId).client
+				res.buildTypeIncomingPeer(requesterId, jsonData.sdp)
+				peerClient.send(res.getResponse())
+			}
+			
+		} else if (jsonData.type == "answerpeer") {
+			const peerId = jsonData.peerId
+
+			// Send answer only if not connecting to itself or target exists
+			if (requesterId != peerId && clients.has(peerId)) {
+				const peerClient = clients.get(peerId).client
+				res.buildTypeAnswerPeer(requesterId, jsonData.sdp)
+				peerClient.send(res.getResponse())
+			}
+		}
+	}catch(e){
+		console.log(e)
+	}
+	
+}
 
 module.exports = {
 	addNewClient:addNewClient
