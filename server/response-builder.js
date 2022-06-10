2c1261115e662a5771d756c713ae5f4a35df7d5b
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-06-10 15:18:00 +0800

    Added build type for incoming and answer peer

--- server/response-builder.js
+++ server/response-builder.js
@@ -2,7 +2,8 @@ const utils = require("./utils")
 
 class ResponseBuilder{
 	static RES_TYPE_INITIAL = "initial"
-	static RES_TYPE_SDP = "sdp"
+	static RES_TYPE_INCOMING_PEER = "incomingpeer"
+	static RES_TYPE_ANSWER_PEER = "answerpeer"
 
 	constructor(){
 		this.response = {}
@@ -15,12 +16,30 @@ class ResponseBuilder{
 	}
 
 
-	// for building the data value in response for type initial
+	// for building the response for type initial
 	buildTypeInitial(id, connectionCreationTime){
 		const response = this.response
 		response.type = ResponseBuilder.RES_TYPE_INITIAL
 		response.id = id
 		response.connectionCreationTime = connectionCreationTime
+	}
+
+
+	// for building the response for type sdp
+	buildTypeIncomingPeer(fromId, sdpData){
+		const response = this.response
+		response.type = ResponseBuilder.RES_TYPE_INCOMING_PEER
+		response.fromId = fromId
+		response.sdp = sdpData
+	}
+
+
+	// for building the response for type sdp
+	buildTypeAnswerPeer(fromId, sdpData){
+		const response = this.response
+		response.type = ResponseBuilder.RES_TYPE_ANSWER_PEER
+		response.fromId = fromId
+		response.sdp = sdpData
 	}
 }
 
