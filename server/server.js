2a53f7cf1a47b0c10f0e2d70f85ecc80a0ab6dff
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-06-10 12:44:50 +0800

    Move signaling logic from server.js to signaling.js

--- server/server.js
+++ server/server.js
@@ -1,29 +1,8 @@
 const ws = require("ws")
-const utils = require("./utils")
+const signaling = require("./signaling.js")
 
 const server = new ws.Server({port:1000})
-const clients = new Map()
 
 server.on("connection", client=>{
-	var id = null
-
-	// prevent id duplicates
-	while(id == null || clients.has(id)){
-		id = utils.uuidv4()
-	}
-
-	const metadata = {
-		id: id,
-		lastTime: utils.getNowMillis()
-	}
-
-	clients.set(client, ws)
-
-	client.on("message", data=>{
-
-	})
-
-	client.on("close", () => {
-		clients.delete(client)
-	})
+	signaling.addNewClient(client)
 })
