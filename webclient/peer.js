fd905a470801feca18d6c806d9a95d90bca5e0d7
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-06-10 12:37:46 +0800

    Convert the provided url to websocket url

--- webclient/peer.js
+++ webclient/peer.js
@@ -1,10 +1,29 @@
 class PeerRTC {
 	
-	constructor(myId, serverURL) {
+	constructor(myId, serverURL) {	
 		this.serverURL = serverURL
+		this.isConnectedToServer = false
+		this.socket = this.initWebSocket()
+	}
+
+
+	initWebSocket(){
+		// Convert the provided server url to web socket url
+
+		const webSocketURL = "ws://" + this.serverURL.replaceAll(/((http(s{0,1}))|(ws(s{0,1}))):\/\//g, "")
 		
-		const socket = new WebSocket(serverURL)
-		this.socket = socket
+		const socket = new WebSocket(webSocketURL)
+
+		socket.onopen= ()=>{
+			this.isConnectedToServer = true
+			console.log("opened")
+		}
+
+		socket.onmessage = data=>{
+			console.log(data)
+		}
+
+		return socket
 	}
 
 	
