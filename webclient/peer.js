7b7dccec23c0c25eeff2db76eddcbd19b0aeacf4
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-06-10 15:18:46 +0800

    Added handling data for incoming and answer peer

--- webclient/peer.js
+++ webclient/peer.js
@@ -1,28 +1,42 @@
 class PeerRTC {
-	
-	constructor(myId, serverURL) {	
+	static REQ_TYPE_CONNECT_PEER = "connectpeer"
+	static REQ_TYPE_ANSWER_PEER = "answerpeer"
+
+	constructor(myId, serverURL, onConnectToServer) {	
 		this.serverURL = serverURL
 		this.isConnectedToServer = false
-		this.socket = this.initWebSocket()
 	}
 
+	connect(peerId){
+		this.socket.send(JSON.stringify({
+			"type": PeerRTC.REQ_TYPE_CONNECT_PEER,
+			"peerId": peerId,
+			"sdp": "test sdp"
+		}))
+	}
 
-	initWebSocket(){
+	start(onConnect){
 		// Convert the provided server url to a web socket url
 		const webSocketURL = "ws://" + this.serverURL.replaceAll(/((http(s{0,1}))|(ws(s{0,1}))):\/\//g, "")
-		
-		const socket = new WebSocket(webSocketURL)
+		new Promise(async(resolve)=>{
+			const socket = new WebSocket(webSocketURL)
+			this.socket = socket
 
-		socket.onopen= ()=>{
-			this.isConnectedToServer = true
-			console.log("opened")
-		}
+			socket.onclose =()=>{
+				console.log("closed")
+			}
 
-		socket.onmessage = data=>{
-			this.handleServerData(data)
-		}
+			socket.onmessage = data=>{
+				this.handleServerData(data)
+			}
 
-		return socket
+			socket.onopen= ()=>{
+				this.isConnectedToServer = true
+				resolve()
+				console.log("opened")
+			}
+		}).then(()=>onConnect(this))
+
 	}
 
 	handleServerData(data){
@@ -30,6 +44,18 @@ class PeerRTC {
 		if (jsonData.type == "initial") {
 			this.id = jsonData.id
 			this.connectionCreationTime = jsonData.connectionCreationTime
+		} else if(jsonData.type == "incomingpeer"){
+			console.log(jsonData.fromId)
+			this.socket.send(JSON.stringify({
+				"type": PeerRTC.REQ_TYPE_ANSWER_PEER,
+				"peerId": jsonData.fromId,
+				"sdp": "answer sdp"
+			}))
+		}
+
+		 else if(jsonData.type == "answerpeer"){
+			console.log(jsonData.fromId)
+			console.log("answering")
 		}
 	}
 
